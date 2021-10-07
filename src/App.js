24e465c0da92711834ab131ef46abd3c59d69e02
rob <robert_regan86@yahoo.com>
2021-10-07 11:11:40 -0400

    put cursor back

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Routes from "./router/Routes";
 import ScrollToTop from "./components/ScrollToTop";
-
+import AnimatedCursor from "react-animated-cursor";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -11,7 +11,14 @@ const App = () => {
   }, []);
   return (
     <>
-     
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={44}
+        color="255, 147, 1"
+        outerAlpha={0.3}
+        innerScale={0.7}
+        outerScale={1.4}
+      />
       <ScrollToTop />
       <Routes />
 
