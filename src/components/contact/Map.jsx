e2458e54df59ca7a91ad473b5fa4d84551802399
rob <robert_regan86@yahoo.com>
2022-09-14 19:57:53 -0400

    update map.jsx

--- src/components/contact/Map.jsx
+++ src/components/contact/Map.jsx
@@ -6,7 +6,7 @@ const Map = () => {
       <div className="google-map">
         <div className="embed-responsive embed-responsive-21by9">
        
-          <iframe className="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24422.008283456307!2d-74.0796530745481!3d40.080965181941544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c185cb14c7c745%3A0x9a42389802fa0026!2sPT%20PLEAS%20BCH%2C%20NJ%2008742!5e0!3m2!1sen!2sus!4v1633002300910!5m2!1sen!2sus"  width="600" height="450"  allowfullscreen="" loading="lazy"></iframe>
+          <iframe className="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5817.772015760965!2d-72.81789972181929!3d43.19090263872229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e05165a359b175%3A0x60c46e19ea9c2450!2sSouth%20Londonderry%2C%20Londonderry%2C%20VT!5e0!3m2!1sen!2sus!4v1663199704902!5m2!1sen!2sus"  width="600" height="450"  allowfullscreen="" loading="lazy"></iframe>
         </div>
       </div>
       {/* End google-map */}
