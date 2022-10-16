d907cb28eb670112b5214efbc9aedd7321044d9a
rob <robert_regan86@yahoo.com>
2022-10-16 09:59:47 -0400

    changed project and updated resume

--- src/components/portfolio/PortfolioAnimation.jsx
+++ src/components/portfolio/PortfolioAnimation.jsx
@@ -1,140 +1,136 @@
-import React from "react";
-import {Tabs, TabPanel } from "react-tabs";
+import React from 'react'
+import { Tabs, TabPanel } from 'react-tabs'
 
-
-import { FiPlus } from "react-icons/fi";
-import Masonry from "react-masonry-css";
+import { FiPlus } from 'react-icons/fi'
+import Masonry from 'react-masonry-css'
 
 const breakpointColumnsObj = {
   default: 3,
   1100: 3,
   700: 2,
   500: 1,
-};
-
+}
 
 const AllPortfolioContent = [
   {
-    img: "feedback",
-    title: "Feedback App",
-    subTitle: "Leave ratings and reviews for products and services!",
-    link: "https://product-review-ui.netlify.app/",
-    code: "https://github.com/robregan/feedback-ui",
+    img: 'feedback',
+    title: 'Feedback App',
+    subTitle: 'Leave ratings and reviews for products and services!',
+    link: 'https://product-review-ui.netlify.app/',
+    code: 'https://github.com/robregan/feedback-ui',
   },
   {
-    img: "gh",
-    title: "GitHub User Search",
-    subTitle: "React App to search user profiles by name or username",
-    link: "https://github-finda.netlify.app/",
-    code: 'https://github.com/robregan/github-user-search' 
+    img: 'gh',
+    title: 'GitHub User Search',
+    subTitle: 'React App to search user profiles by name or username',
+    link: 'https://github-finda.netlify.app/',
+    code: 'https://github.com/robregan/github-user-search',
   },
   {
-    img: "eth2",
-    title: "Web3 App",
-    subTitle: "Send ETH anywhere in the world! A gif and a message will be forever attached to your transaction on the blockchain",
-    link: "https://cryptobobby.digital/",
-    code: 'https://github.com/robregan/web3app' 
+    img: 'eth2',
+    title: 'Web3 App',
+    subTitle:
+      'Send ETH anywhere in the world! A gif and a message will be forever attached to your transaction on the blockchain',
+    link: 'https://cryptobobby.digital/',
+    code: 'https://github.com/robregan/web3app',
   },
   {
-    img: "chill3",
-    title: "Chillow Marketplace",
-   subTitle: "Browse current listings or post your property for sale!",
-   link: "https://chillow.netlify.app/",
-   code: 'https://github.com/robregan/house-market-app' 
+    img: 'chill3',
+    title: 'Chillow Marketplace',
+    subTitle: 'Browse current listings or post your property for sale!',
+    link: 'https://chillow.netlify.app/',
+    code: 'https://github.com/robregan/house-market-app',
   },
   {
-    img: "face9",
-    title: "AI Emotion Analyzer",
-    subTitle: "Users upload a photo and the program will analyze the emotions of detected faces.",
-    link: "https://emotion-check.herokuapp.com/",
-    code: 'https://github.com/robregan/emotion-check' 
+    img: 'face9',
+    title: 'AI Emotion Analyzer',
+    subTitle:
+      'Users upload a photo and the program will analyze the emotions of detected faces.',
+    link: 'https://emotion-check.herokuapp.com/',
+    code: 'https://github.com/robregan/emotion-check',
   },
   {
-    img: "chat4",
-    title: "Medical Pager",
-    subTitle: "Chat app for your workspace with tons of features.",
-    link: "https://employee-chat.netlify.com/",
-    code: 'https://github.com/robregan/employee-chat' 
+    img: 'thumbstore',
+    title: 'ProShop',
+    subTitle: 'Ecommerce store with PayPal integration',
+    link: 'https://ecom-app-802.herokuapp.com/',
+    code: 'https://github.com/robregan/anothaOne',
   },
- 
-  {
-    img: "gypsy",
-    title: "Age Guesser",
-    subTitle: "By using Azure's Computer Vision API, this app will guess your age!",
-    link: "https://gypsy-queen.herokuapp.com/",
-    code: 'https://github.com/robregan/face-analyza' 
-  },
-  {
-    img: "object",
-    title: "Object Detector",
-    subTitle: "Upload an image and our AI will detect any objects.",
-    link: "https://object-detecta.herokuapp.com/",
-    code: 'https://github.com/robregan/object-finder' 
-  },
-  {
-    img: "help",
-    title: "Support Desk",
-    subTitle: "Users can login and create support tickets",
-    link: "https://customer-tickets.herokuapp.com/",
-    code: 'https://github.com/robregan/support-desk' 
-  },
-];
 
+  {
+    img: 'gypsy',
+    title: 'Age Guesser',
+    subTitle:
+      "By using Azure's Computer Vision API, this app will guess your age!",
+    link: 'https://gypsy-queen.herokuapp.com/',
+    code: 'https://github.com/robregan/face-analyza',
+  },
+  {
+    img: 'object',
+    title: 'Object Detector',
+    subTitle: 'Upload an image and our AI will detect any objects.',
+    link: 'https://object-detecta.herokuapp.com/',
+    code: 'https://github.com/robregan/object-finder',
+  },
+  {
+    img: 'help',
+    title: 'Support Desk',
+    subTitle: 'Users can login and create support tickets',
+    link: 'https://customer-tickets.herokuapp.com/',
+    code: 'https://github.com/robregan/support-desk',
+  },
+]
 
 const PortfolioAnimation = () => {
   return (
-    <div className="portfolio-filter-01">
+    <div className='portfolio-filter-01'>
       <Tabs>
         <TabPanel>
-          <div className="portfolio-content ">
+          <div className='portfolio-content '>
             <Masonry
               breakpointCols={breakpointColumnsObj}
-              className="my-masonry-grid"
-              columnClassName="my-masonry-grid_column"
+              className='my-masonry-grid'
+              columnClassName='my-masonry-grid_column'
             >
               {AllPortfolioContent.map((val, i) => (
                 <div
-                  className="portfolio-box-01"
+                  className='portfolio-box-01'
                   key={i}
-                  data-aos="fade-right"
-                  data-aos-duration="1200"
+                  data-aos='fade-right'
+                  data-aos-duration='1200'
                   data-aos-delay={val.delayAnimation}
                 >
-                  <div className="portfolio-img">
+                  <div className='portfolio-img'>
                     <img
                       src={`img/portfolio/${val.img}.jpg`}
                       alt={val.alterText}
                     />
                   </div>
 
-                  <div className="siteCode">
+                  <div className='siteCode'>
                     <h4>
                       <a
                         href={`${val.link}`}
-                        target="_blank"
-                        className="gallery-link"
+                        target='_blank'
+                        className='gallery-link'
                       >
                         Live Site
                       </a>
                     </h4>
 
-                    <p className="siteCode wutang">{val.title}</p>
+                    <p className='siteCode wutang'>{val.title}</p>
 
-                    <h4 className="code">
-                      {" "}
+                    <h4 className='code'>
+                      {' '}
                       <a
                         href={`${val.code}`}
-                        target="_blank"
-                        className="gallery-link"
+                        target='_blank'
+                        className='gallery-link'
                       >
                         Code
                       </a>
-               
-                    </h4>  
-
-                   
+                    </h4>
                   </div>
-                
                 </div>
               ))}
             </Masonry>
@@ -144,7 +140,7 @@ const PortfolioAnimation = () => {
         </TabPanel>
       </Tabs>
     </div>
-  );
-};
+  )
+}
 
-export default PortfolioAnimation;
+export default PortfolioAnimation
--- src/components/slider/SliderAnimation.jsx
+++ src/components/slider/SliderAnimation.jsx
@@ -1,31 +1,27 @@
-import React from "react";
-import TextLoop from "react-text-loop";
+import React from 'react'
+import TextLoop from 'react-text-loop'
 
 const conctInfo = {
- 
-  email: "rsr@robregan.dev",
-};
+  email: 'rsr@robregan.dev',
+}
 
 const sliderContent = {
-  name: "Robert Regan",
-  designation: "Full-stack Developer",
+  name: 'Robert Regan',
+  designation: 'Full-stack Developer',
   description: `I design and develop services for customers of all sizes,
   specializing in creating stylish, modern websites, web
   services and online stores.`,
-  btnText: " My Resume",
-};
+  btnText: ' My Resume',
+}
 
 const Slider = () => {
   return (
     <>
       {/*  Home Banner */}
-      <section id="home" className="home-banner">
-        <div className="hb-top-fixed d-flex">
-          <div className="hb-info">
-           
-            <a href="mailto:rsr@robregan.dev">
-              {conctInfo.email}
-            </a>
+      <section id='home' className='home-banner'>
+        <div className='hb-top-fixed d-flex'>
+          <div className='hb-info'>
+            <a href='mailto:rsr@robregan.dev'>{conctInfo.email}</a>
           </div>
           {/* <div className="hb-lang">
             <ul className="nav">
@@ -40,51 +36,51 @@ const Slider = () => {
         </div>
         {/* End hp-top-fixed */}
 
-        <div className="container">
-          <div className="row full-screen align-items-center">
-            <div className="col-lg-7">
-              <div className="type-box">
-                <h6 data-aos="fade-up" data-aos-duration="1200">
+        <div className='container'>
+          <div className='row full-screen align-items-center'>
+            <div className='col-lg-7'>
+              <div className='type-box'>
+                <h6 data-aos='fade-up' data-aos-duration='1200'>
                   Hello, My name is
                 </h6>
                 <h1
-                  className="font-alt"
-                  data-aos="fade-up"
-                  data-aos-duration="1200"
-                  data-aos-delay="100"
+                  className='font-alt'
+                  data-aos='fade-up'
+                  data-aos-duration='1200'
+                  data-aos-delay='100'
                 >
                   {sliderContent.name}
                 </h1>
                 <div
-                  data-aos="fade-up"
-                  data-aos-duration="1200"
-                  data-aos-delay="200"
+                  data-aos='fade-up'
+                  data-aos-duration='1200'
+                  data-aos-delay='200'
                 >
                   <TextLoop>
-                    <p className="loop-text lead">Full-stack Developer</p>
-                    <p className="loop-text lead"> Avid Snowboarder</p>
-                    <p className="loop-text lead"> Husband and Yogi</p>
+                    <p className='loop-text lead'>Full-stack Developer</p>
+                    <p className='loop-text lead'> Avid Snowboarder</p>
+                    <p className='loop-text lead'> Husband and Yogi</p>
                   </TextLoop>
                 </div>
 
                 <p
-                  className="desc"
-                  data-aos="fade-up"
-                  data-aos-duration="1200"
-                  data-aos-delay="300"
+                  className='desc'
+                  data-aos='fade-up'
+                  data-aos-duration='1200'
+                  data-aos-delay='300'
                 >
                   {sliderContent.description}
                 </p>
                 <div
-                  className="mt-4"
-                  data-aos="fade-up"
-                  data-aos-duration="1200"
-                  data-aos-delay="400"
+                  className='mt-4'
+                  data-aos='fade-up'
+                  data-aos-duration='1200'
+                  data-aos-delay='400'
                 >
                   <a
-                    className="px-btn px-btn-white"
-                    href="https://res.cloudinary.com/storage-b0x/image/upload/v1644872465/r-regan-resume_1_uhg5ru.pdf"
-                    target="_blank"
+                    className='px-btn px-btn-white'
+                    href='https://drive.google.com/file/d/12l-_bM9wgnTqltfJekdUdJ7sbkiWjAD2/view'
+                    target='_blank'
                   >
                     {sliderContent.btnText}
                   </a>
@@ -95,10 +91,10 @@ const Slider = () => {
         </div>
         {/* End Container*/}
         <div
-          className="hb-me"
+          className='hb-me'
           style={{
             backgroundImage: `url(${
-              process.env.PUBLIC_URL + "/img/slider/colorado.jpg"
+              process.env.PUBLIC_URL + '/img/slider/colorado.jpg'
             })`,
           }}
         ></div>
@@ -106,7 +102,7 @@ const Slider = () => {
 
       {/* End Home Banner  */}
     </>
-  );
-};
+  )
+}
 
-export default Slider;
+export default Slider
